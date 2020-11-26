9fb23bcf3ff89d24480e612fc2e741bf458cc09d
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-11-26 17:50:48 +0200

    First commit

---
+++ Project 5.1.cpp
@@ -0,0 +1,30 @@
+﻿// Lab_05_1.cpp
+// < Кузів Назар >
+// Лабораторна робота № 5.1
+// Функції, що містять арифметичний вираз
+// Варіант 6
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double h(const double x, const double y)
+{
+    double S = (x * y / (1 + x * x * y * y));
+    return S;
+}
+
+int main()
+{
+    double s, t;
+
+    cout << " s = "; cin >> s;
+    cout << " t = "; cin >> t;
+
+    double v = (h(s * s, t * t) + pow(h(s + t, 1), 2)) / (1 + pow(h(s * t, 2), 2));
+
+    cout << " v = " << v << endl;
+
+    return 0;
+}
